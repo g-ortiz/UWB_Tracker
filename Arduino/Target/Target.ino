f028359ed4b80fe5dd83f5262f86734bbcf372b0
Gabriel Ortiz <gabriel.betancur@cybercom.com>
2017-01-30 15:20:30 +0100

    Add first files

---
+++ Arduino/Target/Target.ino
@@ -0,0 +1,166 @@
+/*
+ * UWB - Tracker
+ * Target
+ * Gabriel Ortiz 
+ * Fredrik Treven
+ * Adapted from: Decawave DW1000 library for arduino RangingAchor example.
+ */
+
+#include <SPI.h>
+#include <DW1000.h>
+
+// Pins in Arduino M0 Pro
+const uint8_t PIN_RST = 9; // reset pin
+const uint8_t PIN_IRQ = 2; // irq pin
+const uint8_t PIN_SS = 7; // spi select pin
+
+// Expected messages
+#define POLL 0
+#define POLL_ACK 1
+#define RANGE 2
+#define RANGE_REPORT 3
+#define RANGE_FAILED 255
+// message flow state
+volatile byte expectedMsgId = POLL_ACK;
+// message sent/received state
+volatile boolean sentAck = false;
+volatile boolean receivedAck = false;
+// timestamps
+DW1000Time timePollSent;
+DW1000Time timePollAckReceived;
+DW1000Time timeRangeSent;
+// data buffer
+#define LEN_DATA 16
+byte data[LEN_DATA];
+// watchdog and reset period
+uint32_t lastActivity;
+uint32_t resetPeriod = 250;
+// reply times (same on both sides for symm. ranging)
+uint16_t replyDelayTimeUS = 3000;
+
+void setup() {
+    // Setup Code
+    // Begin serial communication
+    Serial.begin(115200);
+    // Set pins and start SPI
+    DW1000.begin(PIN_IRQ, PIN_RST);
+    DW1000.select(PIN_SS);
+    // general configuration
+    DW1000.newConfiguration();
+    DW1000.setDefaults();
+    DW1000.setDeviceAddress(2);
+    DW1000.setNetworkId(10);
+    DW1000.enableMode(DW1000.MODE_LONGDATA_RANGE_LOWPOWER);
+    DW1000.commitConfiguration();
+    DW1000.enableDebounceClock();
+    DW1000.enableLedBlinking();   
+    DW1000.attachSentHandler(handleSent);
+    DW1000.attachReceivedHandler(handleReceived);
+    // Target transmitting a POLL message
+    receiver();
+    transmitPoll();
+    // reset watchdog
+    noteActivity();
+}
+
+void noteActivity() {
+    // reset watchdog
+    lastActivity = millis();
+}
+
+void resetInactive() {
+    // when watchdog times out, reset device
+    expectedMsgId = POLL_ACK;
+    transmitPoll();
+    noteActivity();
+}
+
+void handleSent() {
+    // change state when ACK sent
+    sentAck = true;
+}
+
+void handleReceived() {
+    // change state when ACK received
+    receivedAck = true;
+}
+
+void transmitPoll() {
+    DW1000.newTransmit();
+    DW1000.setDefaults();
+    data[0] = POLL;
+    DW1000.setData(data, LEN_DATA);
+    DW1000.startTransmit();
+}
+
+void transmitRange() {
+    DW1000.newTransmit();
+    DW1000.setDefaults();
+    data[0] = RANGE;
+    // delay sending the message and remember expected future sent timestamp
+    DW1000Time deltaTime = DW1000Time(replyDelayTimeUS, DW1000Time::MICROSECONDS);
+    timeRangeSent = DW1000.setDelay(deltaTime);
+    timePollSent.getTimestamp(data + 1);
+    timePollAckReceived.getTimestamp(data + 6);
+    timeRangeSent.getTimestamp(data + 11);
+    DW1000.setData(data, LEN_DATA);
+    DW1000.startTransmit();
+}
+
+void receiver() {
+    DW1000.newReceive();
+    DW1000.setDefaults();
+    // Enable receiver
+    DW1000.receivePermanently(true);
+    DW1000.startReceive();
+}
+
+void loop() {
+    if (!sentAck && !receivedAck) {
+        // reset if wathcdog timed out
+        if (millis() - lastActivity > resetPeriod) {
+            resetInactive();
+        }
+        return;
+    }
+    // SentAck (after transmitting POLL and RANGE)
+    if (sentAck) {
+        sentAck = false;
+        byte msgId = data[0];
+        if (msgId == POLL) {
+            DW1000.getTransmitTimestamp(timePollSent);
+        } else if (msgId == RANGE) {
+            DW1000.getTransmitTimestamp(timeRangeSent);
+            noteActivity();
+        }
+    }
+    if (receivedAck) {
+        receivedAck = false;
+        // get message and parse
+        DW1000.getData(data, LEN_DATA);
+        byte msgId = data[0];
+        if (msgId != expectedMsgId) {
+            // unexpected message, start over again
+            expectedMsgId = POLL_ACK;
+            transmitPoll();
+            return;
+        }
+        if (msgId == POLL_ACK) {
+            DW1000.getReceiveTimestamp(timePollAckReceived);
+            expectedMsgId = RANGE_REPORT;
+            transmitRange();
+            noteActivity();
+        } else if (msgId == RANGE_REPORT) {
+            expectedMsgId = POLL_ACK;
+            float curRange;
+            memcpy(&curRange, data + 1, 4);
+            transmitPoll();
+            noteActivity();
+        } else if (msgId == RANGE_FAILED) {
+            expectedMsgId = POLL_ACK;
+            transmitPoll();
+            noteActivity();
+        }
+    }
+}
+
